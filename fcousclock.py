f8104712a60dfd3b79c82313678a00f76fc7e460
hhpeo <134208106+hhpeo@users.noreply.github.com>
2023-05-21 21:06:27 +0800

    Create fcousclock.py

---
+++ fcousclock.py
@@ -0,0 +1,14 @@
+import time
+
+def focus_timer(minutes):
+    seconds = minutes * 60
+    while seconds > 0:
+        mins, secs = divmod(seconds, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        seconds -= 1
+
+    print("时间到！")
+
+focus_timer(25) # 设置 25 分钟的专注时间
